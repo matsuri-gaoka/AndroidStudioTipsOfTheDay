ac2ae0f941bdbb4782ed86545a35b1bbfa789fca
matsuri-gaoka <repetitive789++@gmail.com>
2022-08-03 22:02:40 +0900

    JavaからKotlinの呼び出し

--- src/main/java/UtilPackage/Util.java
+++ src/main/java/UtilPackage/Util.java
@@ -1,8 +1,10 @@
 package UtilPackage;
 
+import KotlinUtilPackage.KotlinUtil;
 public class Util {
     public static void SayHelloJava() {
         System.out.println("hello in java");
+        KotlinUtil.Companion.sayHelloKotlin();
     }
 }
 
---
+++ src/main/kotlin/KotlinUtilPackage/KotlinUtil.kt
@@ -0,0 +1,9 @@
+package KotlinUtilPackage
+
+class KotlinUtil {
+    companion object {
+        fun sayHelloKotlin() {
+            println("hello in Kotlin")
+        }
+    }
+}
